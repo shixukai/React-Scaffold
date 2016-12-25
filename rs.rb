12b537a81280a93e1ebeb9422bab7c5d617aff15
施旭凯 <shixukai@163.com>
2016-12-25 15:05:16 +0800

    change dir

--- rs.rb
+++ rs.rb
@@ -19,8 +19,8 @@ if !comp_name
 end
 
 # files_name = ['Template/Template.js', 'Template/styles.scss', 'Template/index.js']
-RsScaffold.sub_and_copy('Template/Template.js', dest_path, 'Template', comp_name, comp_name + '.js')
-RsScaffold.sub_and_copy('Template/index.js', dest_path, 'Template', comp_name)
-RsScaffold.sub_and_copy('Template/styles.scss', dest_path, 'Template', comp_name)
+RsScaffold.sub_and_copy('Template/components/Template.js', dest_path, 'Template', comp_name, comp_name + '.js')
+RsScaffold.sub_and_copy('Template/components/index.js', dest_path, 'Template', comp_name)
+RsScaffold.sub_and_copy('Template/components/styles.scss', dest_path, 'Template', comp_name)
 
 puts "组件创建成功 : #{File.join(dest_path, comp_name)}"
